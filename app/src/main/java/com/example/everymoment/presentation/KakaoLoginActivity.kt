800d14c210fb809bc3d0fff39308ab4c1c8d4961
todlf <todlfdl@naver.com>
2024-09-25 22:03:55 +0900

    feat: 로그인창 XML 생성

---
+++ app/src/main/java/com/example/everymoment/presentation/KakaoLoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.everymoment.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.everymoment.R
+
+class KakaoLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kakao_login)
+    }
+}
