bfeddb880260be58cb2f9cee60a3157c93d8fd7f
todlf <todlfdl@naver.com>
2024-10-03 16:36:55 +0900

    feat: 친구 목록, 친구 찾기 레이아웃 생성

---
+++ app/src/main/java/com/example/everymoment/presentation/view/FriendsListFragment.kt
@@ -0,0 +1,12 @@
+package com.example.everymoment.presentation.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.everymoment.R
+
+class FriendsListFragment : Fragment() {
+
+}
