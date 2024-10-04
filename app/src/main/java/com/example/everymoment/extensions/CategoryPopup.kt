fe3b80e56858c72cc86e9c659dda361e8f444fdc
settle54 <settle54@kangwon.ac.kr>
2024-10-04 19:03:59 +0900

    feat: make custom dialog

---
+++ app/src/main/java/com/example/everymoment/extensions/CategoryPopup.kt
@@ -0,0 +1,126 @@
+package com.example.everymoment.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Color
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.GridLayout
+import android.widget.ImageView
+import android.widget.PopupWindow
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import com.example.everymoment.databinding.CategoryPopupBinding
+import com.example.everymoment.R
+
+class CategoryPopup(
+    private val fragmentActivity: FragmentActivity,
+    private val context: Context
+) {
+
+    private lateinit var categoryPopup: PopupWindow
+    private var categoryCount = 0
+    private lateinit var popupView: CategoryPopupBinding
+    private lateinit var gridLayout: GridLayout
+    private lateinit var addButton: ImageView
+    private val maxRows: Int = 4
+    private val itemsPerRow: Int = 3
+
+    private var listener: ((String?) -> Unit)? = null
+    private lateinit var addCategoryDialog: CustomEditDialog
+
+
+    fun showCategoryPopup(
+        anchorView: View,
+        xOffset: Int = 0,
+        yOffset: Int = 0,
+        onCategorySelected: ((String?) -> Unit)
+    ) {
+        this.listener = onCategorySelected
+
+        if (!::categoryPopup.isInitialized) {
+            setCategryPopup()
+            setAddCategoryDialog()
+        }
+
+        categoryPopup = PopupWindow(
+            popupView.root,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+
+        categoryPopup.isOutsideTouchable = true
+        categoryPopup.isFocusable = true
+
+        val location = IntArray(2)
+        anchorView.getLocationOnScreen(location)
+        categoryPopup.showAtLocation(
+            anchorView,
+            Gravity.NO_GRAVITY,
+            location[0] + xOffset,
+            location[1] + yOffset
+        )
+
+        popupView.root.setOnTouchListener { view, event ->
+            view.parent.requestDisallowInterceptTouchEvent(true)
+            view.onTouchEvent(event)
+            true
+        }
+    }
+
+    private fun setCategryPopup() {
+        popupView = CategoryPopupBinding.inflate((context as Activity).layoutInflater)
+        addButton = popupView.addCategory
+        gridLayout = popupView.categoryGrid
+
+        addButton.setOnClickListener {
+            if (categoryCount < maxRows * itemsPerRow) {
+                addCategoryDialog.show(fragmentActivity.supportFragmentManager, "CustomEditDialog")
+            }
+        }
+    }
+
+    private fun setAddCategoryDialog() {
+        addCategoryDialog = CustomEditDialog(
+            context.resources.getString(R.string.category_add_dialog),
+            context.resources.getString(R.string.example_category),
+            context.resources.getString(R.string.category_add_dialog_instruction),
+            context.resources.getString(R.string.cancel),
+            context.resources.getString(R.string.save),
+            onPositiveClick = {
+                addCategoryTextView(it)
+            })
+    }
+
+    private fun addCategoryTextView(userInput: String) {
+        val textView = TextView(context).apply {
+            layoutParams = GridLayout.LayoutParams().apply {
+                width = GridLayout.LayoutParams.WRAP_CONTENT
+                height = GridLayout.LayoutParams.MATCH_PARENT
+
+                setGravity(Gravity.CENTER)
+                setMargins(10, 10, 20, 20)
+            }
+            background = resources.getDrawable(R.drawable.category_background, null)
+            setPadding(15, 10, 15, 10)
+            setTextColor(Color.WHITE)
+            text = resources.getString(R.string.category_text, userInput)
+
+            setOnClickListener {
+                listener?.invoke(this.text.toString())
+            }
+        }
+        gridLayout.addView(textView)
+        categoryCount += 1
+        moveAddButtonToNext()
+    }
+
+    private fun moveAddButtonToNext() {
+        gridLayout.removeView(addButton)
+        gridLayout.addView(addButton)
+        if (categoryCount >= maxRows * itemsPerRow) {
+            addButton.visibility = View.GONE
+        }
+    }
+}
--- app/src/main/java/com/example/everymoment/presentation/view/SettingFragment.kt
+++ app/src/main/java/com/example/everymoment/presentation/view/SettingFragment.kt
@@ -6,14 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.LinearLayout
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat
-import androidx.core.view.marginStart
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.everymoment.R
 import com.example.everymoment.databinding.FragmentSettingBinding
+import com.example.everymoment.extensions.CustomDialog
+import com.example.everymoment.extensions.CustomEditDialog
 import com.example.everymoment.extensions.GalleryUtil
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
@@ -21,9 +19,11 @@ class SettingFragment : Fragment() {
 
     private lateinit var binding: FragmentSettingBinding
 
-    private val galleryUtil = GalleryUtil(this) { imageUri ->
-        addImage(imageUri)
-    }
+    private val galleryUtil = GalleryUtil(this)
+
+    private lateinit var profileImageDialog: CustomDialog
+    private lateinit var profileNameDialog: CustomDialog
+    private lateinit var nameChangeDialog: CustomEditDialog
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,33 +36,86 @@ class SettingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setDialogs()
+
         binding.accountImage.setOnClickListener {
-            showProfileImageDialog()
+            profileImageDialog.show(requireActivity().supportFragmentManager, "CustomDialog")
         }
 
         binding.cameraButton.setOnClickListener {
-            showProfileImageDialog()
+            profileImageDialog.show(requireActivity().supportFragmentManager, "CustomDialog")
         }
 
         binding.editButton.setOnClickListener {
-            showProfileNameDialog()
+            profileNameDialog.show(requireActivity().supportFragmentManager, "CustomDialog")
+        }
+        
+        binding.notificationToggle.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                Toast.makeText(requireContext(), resources.getString(R.string.notification_isChecked), Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), resources.getString(R.string.notification_isUnChecked), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.timeRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+            var selectedTime = ""
+            when (checkedId) {
+                R.id.time15m -> {
+                    selectedTime = resources.getString(R.string.time_15min)
+                }
+                R.id.time20m -> {
+                    selectedTime = resources.getString(R.string.time_20min)
+                }
+                R.id.time25m -> {
+                    selectedTime = resources.getString(R.string.time_25min)
+                }
+                R.id.time30m -> {
+                    selectedTime = resources.getString(R.string.time_30min)
+                }
+            }
+            Toast.makeText(requireContext(), getString(R.string.time_interval_text, selectedTime), Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun showProfileImageDialog() {
-        MaterialAlertDialogBuilder(requireContext(), R.style.WhiteDialog)
-            .setMessage(resources.getString(R.string.profile_image_dialog))
-            .setNegativeButton(resources.getString(R.string.cancel)) { dialog, _ ->
-                dialog.dismiss()
+    private fun setDialogs() {
+        profileImageDialog =
+            CustomDialog(
+                resources.getString(R.string.profile_image_dialog),
+                resources.getString(R.string.cancel),
+                resources.getString(R.string.change),
+                onPositiveClick = {
+                    galleryUtil.openGallery(onImageSelected = {
+                        addImage(it)
+                    })
+                }).apply {
+                isCancelable = false
             }
-            .setPositiveButton(resources.getString(R.string.profile_image_dialog_posiitve)) { dialog, _ ->
-                galleryUtil.openGallery()
-                dialog.dismiss()
-            }
-            .show()
-    }
 
-    private fun showProfileNameDialog() {
+        profileNameDialog =
+            CustomDialog(
+                resources.getString(R.string.profile_name_dialog),
+                resources.getString(R.string.cancel),
+                resources.getString(R.string.change),
+                onPositiveClick = {
+                    nameChangeDialog.show(
+                        requireActivity().supportFragmentManager,
+                        "CustomEditDialog"
+                    )
+                }).apply {
+                isCancelable = false
+            }
+
+        nameChangeDialog =
+            CustomEditDialog(
+                resources.getString(R.string.change_name_dialog),
+                resources.getString(R.string.example_name),
+                resources.getString(R.string.change_name_dialog_instruction),
+                resources.getString(R.string.cancel),
+                resources.getString(R.string.save),
+                onPositiveClick = {
+                    binding.accountName.text = it
+                })
     }
 
     private fun addImage(imageUri: Uri?) {
