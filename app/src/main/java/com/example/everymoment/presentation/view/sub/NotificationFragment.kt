ea6521d4a782a36f3e29171d9995a928c857b103
arieum <lar713@naver.com>
2024-10-23 01:38:55 +0900

    feat: 알림화면 mvvm 구현

--- app/src/main/java/com/example/everymoment/presentation/view/sub/NotificationFragment.kt
+++ app/src/main/java/com/example/everymoment/presentation/view/sub/NotificationFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.everymoment.data.repository.NotificationRepository
 import com.example.everymoment.databinding.FragmentNotificationBinding
+import com.example.everymoment.presentation.viewModel.NotificationViewModel
+import com.example.everymoment.presentation.viewModel.NotificationViewModelFactory
 
 class NotificationFragment : Fragment() {
 
     private lateinit var binding: FragmentNotificationBinding
+    private lateinit var viewModel: NotificationViewModel
+    private val notificationRepository = NotificationRepository()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +28,9 @@ class NotificationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(this, NotificationViewModelFactory(notificationRepository)).get(
+            NotificationViewModel::class.java
+        )
         binding.backButton.setOnClickListener {
             requireActivity().supportFragmentManager.popBackStack()
         }
---
+++ app/src/main/java/com/example/everymoment/presentation/viewModel/NotificationViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.everymoment.presentation.viewModel
+
+import androidx.lifecycle.ViewModel
+import com.example.everymoment.data.repository.NotificationRepository
+
+class NotificationViewModel(private val notificationRepository: NotificationRepository) : ViewModel() {
+
+}
